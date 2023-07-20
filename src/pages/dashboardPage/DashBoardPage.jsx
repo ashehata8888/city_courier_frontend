63a272d7a3054a9e571657d7527e1fc4b891c76d
Ahmed Shehata <47960845+ashehata8888@users.noreply.github.com>
2023-07-20 13:15:55 +0300

    dashboard almost done

--- src/pages/dashboardPage/DashBoardPage.jsx
+++ src/pages/dashboardPage/DashBoardPage.jsx
@@ -1,18 +1,86 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import AppContext from "../../context/AppContext";
 import Navbar from "../../components/NavBar/NavBar";
+import { MDBTable, MDBTableHead, MDBTableBody } from "mdb-react-ui-kit";
+
+import { ProgressBar } from "react-bootstrap";
+import apiServices from "../../services/apiServices";
 
 export default function DashBoardPage() {
+  const [trackingData, setTrackingData] = useState([]);
   const contextData = useContext(AppContext);
   console.log(contextData);
 
   const userDataPrivilege = "sender";
 
+ 
+  useEffect(() => {
+    const TRACKING_URL = "http://localhost:7000/api/tracking";
+
+    const fetchTrackingData = async () => {
+      try {
+        const response = await apiServices.fetchData(TRACKING_URL);
+        console.log("test Tracking data from Dashboard", response);
+
+        setTrackingData(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchTrackingData();
+  }, []);
+
   return (
     <>
       <Navbar userDataPrivilege={userDataPrivilege} />
-      <h1 style={{ color: "black" }}> DashBoardPage</h1>
-      {/* <p>{userDataPrivilege}</p> */}
+      <h1 style={{ color: "black", marginTop: "150px" }}> </h1>
+
+      <MDBTable>
+        <MDBTableHead light>
+          <tr>
+            <th scope="col">PARCEL NO</th>
+            <th scope="col">PARCEL CONTENT</th>
+            <th scope="col">STATUS</th>
+            <th scope="col">DATE</th>
+
+            {/* <th scope='col'>Handle</th> */}
+          </tr>
+        </MDBTableHead>
+        <MDBTableBody>
+          {console.log("trackingDataTest", trackingData)}
+          {trackingData.length === 0 ? (
+  <tr>
+    <td colSpan="4">Loading...</td>
+  </tr>
+) : (trackingData.map((item) => {
+            console.log("item",item)
+            return (
+            
+                <tr key={item.id}>
+                  <th scope="row" style={{color:"red"}}><h3>{item.id}</h3></th>
+                  <td>{item.parcel_content}</td>
+                  <td>
+                    <ProgressBar
+                      now={50}
+                      label={item.parcel_status}
+                      variant="completed"
+                    />
+                  </td>
+                  <td>{item.picked_up_time}</td>
+                </tr>
+           
+            );
+          }))}
+        </MDBTableBody>
+        {/* <tfoot>
+        <tr>
+          <td>Footer</td>
+          <td>Footer</td>
+          <td>Footer</td>
+          <td>Footer</td>
+        </tr>
+      </tfoot> */}
+      </MDBTable>
     </>
   );
 }
--- src/services/apiServices.jsx
+++ src/services/apiServices.jsx
@@ -1,97 +1,84 @@
+import AppContext from "../context/AppContext";
+import React, { useContext } from "react";
 
-import AppContext from '../context/AppContext';
-import React,{ useContext} from 'react';
-
-
-
-
-
-const API_URL = 'http://localhost:7000/api/users/authenticate'
+const API_URL = "http://localhost:7000/api/users/authenticate";
 const TRACKING_URL = "http://localhost:7000/api/tracking";
 
-async function sendRequest(url,options) {
- const response = await fetch(url,options);
- return await response.json();
+function createRequestOptionsJWT(method, body) {
+  return {
+    method,
+    headers: new Headers({
+      Authorization: "bearer " + userDataJS.token,
+      "Content-Type": "application/json",
+    }),
+    withCredentials: true,
+    body: JSON.stringify(body),
+  };
 }
 
+function fetchOptionsWithoutBody(method) {
+  return {
+    method,
+    headers: new Headers({
+      Authorization: "bearer " + userDataJS.token,
+      "Content-Type": "application/json",
+    }),
+    withCredentials: true,
+  };
+}
+
+async function sendRequest(url, options) {
+  const response = await fetch(url, options);
+  return await response.json();
+}
 
 const userData = localStorage.getItem("userData");
-const userDataJS = JSON.parse(userData)
+const userDataJS = JSON.parse(userData);
 
-console.log("contextUserData from apiServices",userDataJS.token)
-
+console.log("contextUserData from apiServices", userDataJS.token);
 
 function createRequestOptions(method, body) {
-
-
- return{
- method,
- headers: {
-  'Content-Type': 'application/json'
-},
-withCredentials: true,
-body: JSON.stringify(body),
-};
+  return {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+    body: JSON.stringify(body),
+  };
 }
 
-function createRequestOptionsJWT(method, body) {
-  return{
-  method,
-  headers: new Headers({
-    Authorization: "bearer " + userDataJS.token,
-    "Content-Type": 'application/json',
-  }),
- withCredentials: true,
- body: JSON.stringify(body),
- };
- }
- 
 //  "Content-Type": "application/x-www-form-urlencoded",
 
-
-
-
-
-
-
-async function fetchUsers(url,options) {
- return await sendRequest(url,options);
+async function fetchData(url) {
+  const options = fetchOptionsWithoutBody("GET")
+  return await sendRequest(url, options);
 }
 
-async function authUser(userCredentials){
-  const options = createRequestOptions('POST',userCredentials);
+async function authUser(userCredentials) {
+  const options = createRequestOptions("POST", userCredentials);
   return await sendRequest(API_URL, options);
- }
-
+}
 
 async function create(user) {
- const options = createRequestOptionsJWT('POST',user);
- return await sendRequest(TRACKING_URL, options);
+  const options = createRequestOptionsJWT("POST", user);
+  return await sendRequest(TRACKING_URL, options);
 }
 
-async function update(userId, user , API_URL) {
- const options = createRequestOptionsJWT('PATCH',user);
- return await sendRequest (`${API_URL}/${userId}`, options);
+async function update(userId, user, API_URL) {
+  const options = createRequestOptionsJWT("PATCH", user);
+  return await sendRequest(`${API_URL}/${userId}`, options);
 }
 
-async function deleteAny(userId , API_URL) {
- const options = createRequestOptionsJWT('DELETE');
- await sendRequest(`${API_URL}/${userId}`,options);
+async function deleteAny(userId, API_URL) {
+  const options = createRequestOptionsJWT("DELETE");
+  await sendRequest(`${API_URL}/${userId}`, options);
 }
 
 export default {
-fetchUsers,
-create,
-update,
-deleteAny,
-authUser
-}
-
-
-
-
-
-
-
-
-
+  fetchData,
+  create,
+  update,
+  deleteAny,
+  authUser,
+};
